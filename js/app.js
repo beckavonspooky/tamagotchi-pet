4a21e2ed7183d5709f62c0914c6b4618e3cf8214
Rebeca Levy <beckalevy@Rebecas-MacBook-Pro.local>
2019-10-01 13:51:26 -0700

    create class/instantiate

---
+++ js/app.js
@@ -0,0 +1,12 @@
+class Tomagotchi {
+    constructor(hunger, sleepiness, boredom, age)
+    this.hunger = hungry;
+    this.sleepiness 
+
+    greet(){
+        console.log('Kon-nichiwa')
+    }
+};
+
+const henry = new Tomagotchi();
+henry.greet();
