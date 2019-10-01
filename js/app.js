2e4a1c46b68b12b23e89d494496bd434bf75154b
Rebeca Levy <beckalevy@Rebecas-MacBook-Pro.local>
2019-10-01 15:44:11 -0700

    set up button

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 class Tomagotchi {
-    constructor(hunger, sleepiness, boredom, age)
-    this.hunger = hungry;
-    this.sleepiness 
-
+    constructor (name){ //the user can choose
+    this.name = name;
+    this.hunger = 0;
+    this.sleepiness = 0;
+    this.boredom = 0;
+    this.age = 0;
+    }
     greet(){
         console.log('Kon-nichiwa')
     }
+    getHunger(){
+
+    }
+    getSleepiness(){
+
+    }
+    getBoredom(){
+
+    }
 };
 
-const henry = new Tomagotchi();
+const henry = new Tomagotchi();//instantiate your tomagotchi
 henry.greet();
+
